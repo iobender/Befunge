2f58ba7fe393e68c33310a094e1636001d9fc145
Matthew Bender <me@iobender.com>
2014-05-29 19:58:46 -0400

    added positon struct

--- befunge.c
+++ befunge.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
+struct position {
+	int row;
+	int col;
+}
+
 struct befunge_program {
 	char code[80][25]; /* befunge code */
 	struct befunge_stack stack; /* stack for data */
-	struct pos ip; /* instruction pointer */
+	struct position ip; /* instruction pointer */
 	struct direction dir; /* execution flow direction */
 }
