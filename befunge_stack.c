2ad31b0337b75c2f286e707a28d81700ebe1e0c5
Matthew Bender <me@iobender.com>
2014-05-29 19:24:33 -0400

    implementation of stack methods, some signature changes in case of allocation errors

--- befunge_stack.c
+++ befunge_stack.c
@@ -1,17 +1,49 @@
+#include <stdlib.h>
 #include "befunge_stack.h"
 
-void bf_init(struct befunge_stack * stack) {
+#define DEFAULT_INIT_STACK_SIZE 64
+
+int bf_init(struct befunge_stack * stack) {
+	stack= malloc(sizeof(struct befunge_stack));
+	if(!stack) return 0;
+	stack->size= 0;
+	stack->capacity= DEFAULT_INIT_STACK_SIZE;
+	stack->data= malloc(stack->capacity * sizeof(int));
+	return stack->data != NULL;
 }
 
-void bf_push(struct befunge_stack * stack, int elt) {
+int bf_push(struct befunge_stack * stack, int elt) {
+	/* check if we need more space for stack. Separate into own function */
+	if(stack->size == stack->capacity) {
+		int * new= realloc(stack->data, 2 * stack->capacity * sizeof(int));
+		if(new) {
+			stack->data= new;
+		} else {
+			return 0;
+		}
+	}
+	stack->data[stack->size++]= elt;
+	return 1;
 }
 
 int bf_pop(struct befunge_stack * stack) {
+	if(stack->size) {
+		return stack->data[--stack->size];
+	} else {
+		return 0; /* popping from an empty stack returns 0 */
+	}
 }
 
 int bf_peek(struct befunge_stack * stack) {
+	if(stack->size) {
+		return stack->data[stack->size - 1];
+	} else {
+		return 0; /* peeking at an empty stack returns 0 */
+	}
 }
 
 void bf_destroy(struct befunge_stack * stack) {
+	free(stack->data);
+	free(stack);
 }
 
--- befunge_stack.h
+++ befunge_stack.h
@@ -4,11 +4,12 @@
 struct befunge_stack {
 	int * data;
 	int size;
-}
+	int capacity;
+};
 
-void bs_init(struct befunge_stack * stack);
+int bs_init(struct befunge_stack * stack);
 
-void bs_push(struct befunge_stack * stack, int elt);
+int bs_push(struct befunge_stack * stack, int elt);
 int bs_pop(struct befunge_stack * stack);
 int bs_peek(struct befunge_stack * stack);
 
